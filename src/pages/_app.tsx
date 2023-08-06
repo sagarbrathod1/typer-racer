9478cfd07829470a3557a3febb9a90cfef865a07
Sagar Rathod <97072541+sagarbrathod1@users.noreply.github.com>
2023-08-06 15:13:19 -0400

    adding clerk for auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
-import "@/styles/globals.css";
-import { ThemeProvider } from "next-themes";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css';
+import { ClerkProvider } from '@clerk/nextjs';
+import { ThemeProvider } from 'next-themes';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider defaultTheme="light" attribute="class" enableSystem={false}>
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
+    return (
+        <ClerkProvider>
+            <ThemeProvider defaultTheme="light" attribute="class" enableSystem={false}>
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </ClerkProvider>
+    );
 }
