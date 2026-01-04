2f2b2e256e9173ee06c04d7d6546743b9a37659e
Sagar Rathod <97072541+sagarbrathod1@users.noreply.github.com>
2026-01-04 16:10:52 -0500

    convex update

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -8,33 +8,46 @@
  * @module
  */
 
+import type * as corpus from "../corpus.js";
+import type * as leaderboard from "../leaderboard.js";
+import type * as sessions from "../sessions.js";
+
 import type {
   ApiFromModules,
   FilterApi,
   FunctionReference,
 } from "convex/server";
-import type * as corpus from "../corpus.js";
-import type * as leaderboard from "../leaderboard.js";
-import type * as sessions from "../sessions.js";
+
+declare const fullApi: ApiFromModules<{
+  corpus: typeof corpus;
+  leaderboard: typeof leaderboard;
+  sessions: typeof sessions;
+}>;
 
 /**
- * A utility for referencing Convex functions in your app's API.
+ * A utility for referencing Convex functions in your app's public API.
  *
  * Usage:
  * ```js
  * const myFunctionReference = api.myModule.myFunction;
  * ```
  */
-declare const fullApi: ApiFromModules<{
-  corpus: typeof corpus;
-  leaderboard: typeof leaderboard;
-  sessions: typeof sessions;
-}>;
 export declare const api: FilterApi<
   typeof fullApi,
   FunctionReference<any, "public">
 >;
+
+/**
+ * A utility for referencing Convex functions in your app's internal API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = internal.myModule.myFunction;
+ * ```
+ */
 export declare const internal: FilterApi<
   typeof fullApi,
   FunctionReference<any, "internal">
 >;
+
+export declare const components: {};
